06527b2504e140a5504a95e5c63bb540bf3340a3
rakeshkandhi <rakeshkandi12345@gmail.com>
2022-11-24 19:23:24 +0530

    stack

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import  { ByeWorld,HelloWorld}  from './companents/HelloWorld';
+import { useState } from 'react';
 function App() {
+  var counter = 0
+  const increment = () => {
+    counter =counter+1
+    console.log(counter)
+  }
+
+
+
   return (
     <div className="App">
       <h1>Hello World </h1>
       <h2></h2>
       <HelloWorld text="This is Hello world " />
       <ByeWorld text="This is bye world "/>
+      <button onClick={increment}>Click me!</button>
+      <h1>{counter}</h1>
     </div>
   );
 }
