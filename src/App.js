3d1335f508319569168e7c82a026eaa37891b559
rakeshkandhi <rakeshkandi12345@gmail.com>
2022-11-24 18:55:13 +0530

    Stack

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import  { ByeWorld }  from './companents/HelloWorld';
+import  { HelloWorld }  from './companents/HelloWorld';
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World </h1>
+      <h2></h2>
+      <HelloWorld />
+      <ByeWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/companents/HelloWorld.jsx
@@ -0,0 +1,10 @@
+export function HelloWorld(){
+    return (
+        <h3>Container jsx</h3>
+    )
+}
+export function ByeWorld(){
+    return (
+        <h3>Bye World</h3>
+    )
+}
